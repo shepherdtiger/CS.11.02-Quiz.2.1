5cf32caff5777915452ae1b8860be86f1ffccd0a
ltst <ltst7232410@gmail.com>
2023-09-21 09:03:36 +0800

    Tiger Y11 quiz 2.1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Give me a number");
+        int numOne=sc.nextInt();
+        //System.out.println(numOne);
+
+        Scanner scII=new Scanner(System.in);
+        System.out.println("Give me another number");
+        int numTwo=scII.nextInt();
+        //System.out.println(numTwo);
+        System.out.println(add(numOne,numTwo));
+    }
+    public static int add(int numOne, int numTwo) {
+        return numOne + numTwo;
     }
 
+    public String greeting(String name) {
+        return "Bonjour+name!";
+    }
+
+    public static int add(int numOne+int numTwo+int numThree+int numFour){
+        return numOne+numTwo+numThree+numFour
+    }
+
+    public printMe
 }
